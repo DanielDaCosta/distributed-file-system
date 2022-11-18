1d206b57941a2fb20cce226395fbf5cbb5be5e4e
Miao-Long <myau.christopher@gmail.com>
2022-11-17 21:36:43 -0800

    Copied over changes from old branch

---
+++ PMR/pmr.py
@@ -0,0 +1,39 @@
+import sys
+sys.path.insert(1, '../edfs')
+import sqledfs
+from enum import Enum
+
+# class syntax
+class EDFS(Enum):
+    MYSQL = 1
+    FIREBASE = 2
+    MONGODB = 3
+
+#TODO maybe push this to the sqledfs.py file?
+def mapPartition(key:str, col_data, data:str):
+    """
+        Arg: The name of the partition from getPartitionLocations
+        col_data: the names of the columns
+        data: the contents of the data itself
+    """
+    return None
+
+def execute(implementation:enum):
+    #TODO import getPartitionLocations() from each
+    if implementation == EDFS.MYSQL:
+        mydb = ccnx.connect(
+          host="localhost",
+          user="root",
+          password="BLANK",
+        )
+        sqledfs.start_env("edfs")
+
+    return None
+
+if __name__ == "__main__":
+
+    if sys.argv[1] == "mysql":
+        #python connector setup
+
+
+        execute(EDFS.MYSQL)
